5acba00f6a243b7b42d8a13db25e0dbcde85018d
Vadim Shikhovtsov <vadimx30@gmail.com>
2024-10-16 21:04:18 +0300

    Add user field to Snippet

--- MainApp/views.py
+++ MainApp/views.py
@@ -24,7 +24,10 @@ def add_snippet_page(request):
     if request.method == "POST":
         form = SnippetForm(request.POST)
         if form.is_valid():
-            form.save()
+            snippet = form.save(commit=False)
+            if request.user.is_authenticated:
+                snippet.user = request.user
+                snippet.save()
             # GET /snippets/list
             return redirect("snippets-list") # URL для списка сниппетов
         return render(request,'pages/add_snippet.html', {'form': form})
