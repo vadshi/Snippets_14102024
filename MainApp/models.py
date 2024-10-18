f18b411ab8a1effb4cc8962daeaec3ce73d049d1
Vadim Shikhovtsov <vadimx30@gmail.com>
2024-10-18 20:00:37 +0300

    Add admin panel

--- MainApp/models.py
+++ MainApp/models.py
@@ -12,15 +12,18 @@ LANGS = (
 
 class Snippet(models.Model):
     name = models.CharField(max_length=100)
-    lang = models.CharField(max_length=30, choices=LANGS)
+    lang = models.CharField(max_length=30, choices=LANGS, verbose_name="Язык")
     code = models.TextField(max_length=5000)
-    creation_date = models.DateTimeField(auto_now_add=True)
+    creation_date = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     user = models.ForeignKey(to=User, on_delete=models.CASCADE, blank=True, null=True)
     public = models.BooleanField(default=True)  # True = public, False = private
 
     def __repr__(self):
         return f'Snippet({self.name})'
 
+    def __str__(self):
+        return f'{self.name}'
+    
 
 class Comment(models.Model):
     text = models.TextField(max_length=1000, verbose_name="Текст комментария")
